a319862edf3c833d27a212aa6bcbb7d42992e999
lipezaballa <lipezaballa@gmail.com>
2023-07-22 11:14:44 +0200

    revert tcu control

--- src/pages/Tube/Tube.tsx
+++ src/pages/Tube/Tube.tsx
@@ -5,10 +5,12 @@ import { DoubleGauge } from "components/DoubleGauge/DoubleGauge";
 import { ReactComponent as TubeRealistic } from "assets/icons/tube-realistic.svg";
 import { useMeasurements } from "hooks/useMeasurements";
 import { selectLcuMeasurements } from "common";
+//import { selectLcuMeasurements, selectTcuMeasurements } from "common";
 
 export const Tube = () => {
     const measurements = useMeasurements();
     const lcuData = selectLcuMeasurements(measurements);
+    //const tcuData = selectTcuMeasurements(measurements);
 
     return (
         <div className={styles.tube}>
@@ -19,10 +21,12 @@ export const Tube = () => {
                 title="Atlas"
                 body="Atlas is Hyperloop UPV's eight edition infrastructure. It provides the vehicle with a low pressure environment to levitate. This is some filler text to make the page take up as much space as possible. It doesn't mean anything nor it should be interpreted to mean something."
             />
-            {/* FIXME: Change this data, but is not tcuMeasurements */}
+            {/* FIXME: Change this data, but is not tcuMeasurements, CORRECT GAUGE SECTION */}
             <DoubleGauge
+                // firstGauge={tcuData.pressure}
+                // secondGauge={tcuData.temperature}
                 firstGauge={lcuData.airgap_1}
-                secondGauge={lcuData.airgap_1}
+                secondGauge={lcuData.airgap_2}
             />
             <Pump on={true} />
         </div>
